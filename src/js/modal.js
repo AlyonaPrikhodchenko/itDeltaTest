cca11caf4cbb76d9aa7ef1c7f0b5eb7770aacfba
Alyona <alenka.inform@gmail.com>
2022-08-19 17:54:45 +0300

    Add mock data

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,13 @@
 import { photoList} from "./create-card.js";
+// import { mock } from "./mock.js";
 import { isEscapeKey } from "./util.js";
 
 const modal = document.querySelector('.modal');
+// const commentList = document.querySelector('.modal__photo-comments');
 
 const openModal = () => {
   photoList.addEventListener('click', (evt) => {
-    if (evt.target) {
+    if (evt.target && evt.target.tagName != 'UL') {
       modal.classList.remove('modal--hidden');
     }
   })
